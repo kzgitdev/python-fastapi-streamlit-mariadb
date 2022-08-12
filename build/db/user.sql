905c7aeb3be087b13ffbac4e68ef30a8b1701e7a
Kazuya.i <kzgitdev@outlook.com>
2022-08-13 00:11:19 +0900

    Update user.sql

--- build/db/user.sql
+++ build/db/user.sql
@@ -8,5 +8,6 @@ CREATE TABLE actress(
   PRIMARY KEY (id)
 );
 
-INSERT INTO actress(name)
-VALUES (花岡エリカ), (川合勇), (澤田かおり), (藤森砂羽), (結城亮), (八木サダヲ), (吉崎竜也), (城咲俊介), (末永豊);
+INSERT INTO user(name)
+VALUES ('花岡エリカ'), ('川合勇'), ('澤田かおり'), ('藤森砂羽'), ('結城亮'), ('八木サダヲ'), ('吉崎竜也'), ('城咲俊介'), ('末永豊');
+
